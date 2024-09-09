ab4dbb22056818a88eea4ef70c8b015ae321aa97
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-09-09 10:30:39 +0200

    Update comment

--- .github/scripts/comment-on-changes.mjs
+++ .github/scripts/comment-on-changes.mjs
@@ -2,24 +2,51 @@ import { Octokit } from "@octokit/action"
 
 // Get the required environment variables (PR number and GitHub token)
 const prNumber = process.argv[2]
-const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split("/")
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/")
 // const token = process.env.GITHUB_TOKEN
 
 const octokit = new Octokit({
   // auth: token,
 })
 
+const IDENTIFIER = "<!-- POMBOT_IDENTIFIER -->"
+
+// find all comments
+const { data: comments } = await octokit.issues.listComments({
+  owner,
+  repo,
+  issue_number,
+})
+
+const prevComment = comments.find((comment) =>
+  comment.body.startsWith(IDENTIFIER),
+)
+
 async function createComment() {
   try {
-    const comment = {
-      owner: repoOwner,
-      repo: repoName,
-      issue_number: prNumber,
-      body: "Hello, this is an automated comment from a GitHub Action!",
+    const body = `${IDENTIFIER}
+TODO find changesets ${Date.now()}
+`
+
+    if (prevComment) {
+      // Update the comment if it already exists
+      await octokit.issues.updateComment({
+        owner,
+        repo,
+        comment_id: prevComment.id,
+        body,
+      })
+      console.log(`Comment updated on PR #${prNumber}`)
+      return
     }
 
     // Create a comment on the pull request
-    await octokit.issues.createComment(comment)
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: prNumber,
+      body,
+    })
     console.log(`Comment created on PR #${prNumber}`)
   } catch (error) {
     console.error(`Failed to create comment: ${error.message}`)
