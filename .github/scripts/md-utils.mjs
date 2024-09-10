a75d79ea4e8eeac2d15be5d80b0d14719fef4b23
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-09-10 11:18:57 +0200

    Fix

--- .github/scripts/md-utils.mjs
+++ .github/scripts/md-utils.mjs
@@ -1,8 +1,7 @@
 import unified from "unified"
 import remarkParse from "remark-parse"
 import remarkStringify from "remark-stringify"
-// @ts-ignore
-import mdastToString from "mdast-util-to-string"
+import * as mdastToString from "mdast-util-to-string"
 
 const BumpLevels = {
   dep: 0,
